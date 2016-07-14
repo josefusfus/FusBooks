e55a1b5a81721ed8289fa7155cda079b90d1b755
Jose Luis Saez Sanchez <Josefus@macbook-pro-de-jose.local>
2016-07-14 20:21:26 +0200

    Initial Commit

---
+++ fusBooks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fusBooks
+//
+//  Created by Jose Luis Saez Sanchez on 14/7/16.
+//  Copyright © 2016 josefus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
